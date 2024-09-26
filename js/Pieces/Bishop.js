d0645d15af021d882dc11075b89f5c8cdf802353
parasite-68 <ansuman.23bcs10072@ms.sst.scaler.com>
2024-09-26 22:56:02 +0530

    Add bishop move logic and integrate with Chess class

--- js/Pieces/Bishop.js
+++ js/Pieces/Bishop.js
@@ -1,11 +1,41 @@
 import Piece from "./Piece.js";
+import { posinbounds } from "../utility.js";
+
 class Bishop extends Piece {
     constructor(color, pos) {
         super(color, pos);
         this.type = 'bishop';
     }
-    moves() {
-        //TODO: Implement how to move Bishop.
+
+    moves(board) {
+        const directions = [
+            { x: 1, y: 1 },
+            { x: 1, y: -1 },
+            { x: -1, y: 1 },
+            { x: -1, y: -1 }
+        ];
+        let possibleMoves = [];
+
+        for (let direction of directions) {
+            let x = this.pos.x + direction.x;
+            let y = this.pos.y + direction.y;
+
+            while (posinbounds({ x, y })) {
+                if (board[x][y] === null) {
+                    possibleMoves.push({ x, y });
+                } else if (board[x][y].color !== this.color) {
+                    possibleMoves.push({ x, y });
+                    break;
+                } else {
+                    break;
+                }
+                x += direction.x;
+                y += direction.y;
+            }
+        }
+
+        return possibleMoves;
     }
 }
+
 export default Bishop;
--- js/Pieces/Pawn.js
+++ js/Pieces/Pawn.js
@@ -1,11 +1,43 @@
 import Piece from './Piece.js';
+import { posinbounds } from '../utility.js';
+
 class Pawn extends Piece {
     constructor(color, pos) {
         super(color, pos);
-        super.type = 'pawn';
+        this.type = 'pawn';
     }
-    moves() {
-        //TODO:Implement pawn.
+
+    moves(board) {
+        let possibleMoves = [];
+        let direction = this.color ? 1 : -1; // White moves up (-1), Black moves down (1)
+        let startRow = this.color ? 1 : 6; // Starting row for white and black pawns
+
+        // Move forward one square
+        let forwardPos = { x: this.pos.x + direction, y: this.pos.y };
+        if (posinbounds(forwardPos) && board[forwardPos.x][forwardPos.y] === null) {
+            possibleMoves.push(forwardPos);
+
+            // Move forward two squares if on starting row
+            if (this.pos.x === startRow) {
+                let doubleForwardPos = { x: this.pos.x + 2 * direction, y: this.pos.y };
+                if (board[doubleForwardPos.x][doubleForwardPos.y] === null) {
+                    possibleMoves.push(doubleForwardPos);
+                }
+            }
+        }
+
+        // Capture diagonally
+        let captureLeft = { x: this.pos.x + direction, y: this.pos.y - 1 };
+        let captureRight = { x: this.pos.x + direction, y: this.pos.y + 1 };
+        if (posinbounds(captureLeft) && board[captureLeft.x][captureLeft.y] !== null && board[captureLeft.x][captureLeft.y].color !== this.color) {
+            possibleMoves.push(captureLeft);
+        }
+        if (posinbounds(captureRight) && board[captureRight.x][captureRight.y] !== null && board[captureRight.x][captureRight.y].color !== this.color) {
+            possibleMoves.push(captureRight);
+        }
+
+        return possibleMoves;
     }
 }
+
 export default Pawn;
