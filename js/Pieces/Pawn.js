609e88c02447c0959076fbdeb94259f1573d2205
parasite-68 <ansuman.23bcs10072@ms.sst.scaler.com>
2024-09-26 22:56:02 +0530

    Add bishop move logic and integrate with Chess class

--- js/Pieces/Pawn.js
+++ js/Pieces/Pawn.js
@@ -5,7 +5,71 @@ class Pawn extends Piece {
         super.type = 'pawn';
     }
     moves() {
-        //TODO:Implement pawn.
+        let moves = [];
+        let x = this.pos.x;
+        let y = this.pos.y;
+        let bobj = window.bobj;
+        if(this.color) // black
+        {
+            if (x === 1)
+            {
+                if (!bobj.getpiece({x: x + 1, y}))
+                {
+                    moves.push({x: x + 1, y});
+                    if (!bobj.getpiece({x: x + 2, y}))
+                    {
+                        moves.push({x: x + 2, y});
+                    }
+                }
+            } 
+            else
+            {
+                if (!bobj.getpiece({x: x + 1, y}))
+                {
+                    moves.push({x: x + 1, y});
+                }
+            }
+            // Capture
+            if (bobj.getpiece({x: x + 1, y: y + 1}) && bobj.getpiece({x: x + 1, y: y + 1}).color !== this.color)
+            {
+                moves.push({x: x + 1, y: y + 1});
+            }
+            if (bobj.getpiece({x: x + 1, y: y - 1}) && bobj.getpiece({x: x + 1, y: y - 1}).color !== this.color)
+            {
+                moves.push({x: x + 1, y: y - 1});
+            }
+        }
+        else
+        {
+            if (x === 6)
+            {
+                if (!bobj.getpiece({x: x - 1, y}))
+                {
+                    moves.push({x: x - 1, y});
+                    if (!bobj.getpiece({x: x - 2, y}))
+                    {
+                        moves.push({x: x - 2, y});
+                    }
+                }
+            } 
+            else
+            {
+                if (!bobj.getpiece({x: x - 1, y}))
+                {
+                    moves.push({x: x - 1, y});
+                }
+            }
+            // Capture
+            if (bobj.getpiece({x: x - 1, y: y + 1}) && bobj.getpiece({x: x - 1, y: y + 1}).color !== this.color)
+            {
+                moves.push({x: x - 1, y: y + 1});
+            }
+            if (bobj.getpiece({x: x - 1, y: y - 1}) && bobj.getpiece({x: x - 1, y: y - 1}).color !== this.color)
+            {
+                moves.push({x: x - 1, y: y - 1});
+            }
+        }
+        return moves;
     }
 }
 export default Pawn;
