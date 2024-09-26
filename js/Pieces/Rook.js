33dce1a44809ad4a90c842324dad31b252d27913
BHAV0207 <bhavyajain4274@gmail.com>
2024-09-26 22:55:01 +0530

    Updated Rook

--- js/Pieces/Rook.js
+++ js/Pieces/Rook.js
@@ -5,8 +5,37 @@ class Rook extends Piece {
         this.type = 'rook';
     }
     moves() {
-        //TODO: Implement knight.
+        let possibleMoves = [];
 
+        let directions = [
+            { dx: 1, dy: 0 },
+            { dx: 0, dy: 1 },
+            { dx: -1, dy: 0 },
+            { dx: 0, dy: -1 }
+        ];
+        
+        for (let direction of directions) {
+            let x = this.pos.x + direction.dx;
+            let y = this.pos.y + direction.dy;
+            
+            while (posinbounds({ x, y })) {
+                let piece = window.bobj.getpiece({ x, y });
+                
+                if (piece) {
+                    if (piece.color !== this.color) {
+                        possibleMoves.push({ x, y });
+                    }
+                    break;
+                }
+                
+                possibleMoves.push({ x, y });
+                x += direction.dx;
+                y += direction.dy;
+            }
+        }
+        
+        return possibleMoves;
+        
     }
 }
 
