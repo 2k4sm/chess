deaf2795c54ff0f4d6fc608561d1e53c105223a2
BHAV0207 <bhavyajain4274@gmail.com>
2024-09-26 22:59:21 +0530

    changes

--- js/Pieces/Rook.js
+++ js/Pieces/Rook.js
@@ -1,4 +1,5 @@
 import Piece from "./Piece.js";
+import { posinbounds } from '../utility.js';
 class Rook extends Piece {
     constructor(color, pos) {
         super(color, pos);
@@ -6,36 +7,27 @@ class Rook extends Piece {
     }
     moves() {
         let possibleMoves = [];
-
-        let directions = [
-            { dx: 1, dy: 0 },
-            { dx: 0, dy: 1 },
-            { dx: -1, dy: 0 },
-            { dx: 0, dy: -1 }
-        ];
-        
-        for (let direction of directions) {
-            let x = this.pos.x + direction.dx;
-            let y = this.pos.y + direction.dy;
-            
-            while (posinbounds({ x, y })) {
-                let piece = window.bobj.getpiece({ x, y });
-                
-                if (piece) {
-                    if (piece.color !== this.color) {
-                        possibleMoves.push({ x, y });
-                    }
+        let dx = [1, 0, -1, 0];
+        let dy = [0, 1, 0, -1];
+        for (let i = 0; i < 4; i++)
+        {
+            let x = this.pos.x + dx[i];
+            let y = this.pos.y + dy[i];
+            while (posinbounds({x, y}))
+            {
+                let piece = window.bobj.getpiece({x, y});
+                if(piece)
+                {
+                    if(piece.color !== this.color)
+                        possibleMoves.push({x, y});
                     break;
                 }
-                
-                possibleMoves.push({ x, y });
-                x += direction.dx;
-                y += direction.dy;
+                possibleMoves.push({x, y});
+                x += dx[i];
+                y += dy[i];
             }
         }
-        
-        return possibleMoves;
-        
+        return possibleMoves;   
     }
 }
 
