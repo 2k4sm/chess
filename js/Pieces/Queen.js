2d0c83df1a321951905936184accf32538751839
spoorthy madasu <spoorthymadasu@spoorthys-MacBook-Air.local>
2024-09-26 23:01:12 +0530

    Implemented queen logic

--- js/Pieces/Queen.js
+++ js/Pieces/Queen.js
@@ -1,14 +1,39 @@
+import { posinbounds } from '../utility.js';
 import Piece from "./Piece.js";
+
 class Queen extends Piece {
     constructor(color, pos) {
         super(color, pos);
         this.type = 'queen';
     }
 
-    // Queen can move in any direction any number of squares
     moves() {
-        //TODO: Implement knight.
+        let possibleMoves = [];
+        let dx = [1, 1, -1, -1, 1, 0, -1, 0];
+        let dy = [1, -1, 1, -1, 0, 1, 0, -1];
+     for (let i = 0; i < 8; i++) {
+            let x = this.pos.x + dx[i];
+            let y = this.pos.y + dy[i];
 
+            while (this.posinbounds({ x, y })) {
+                let piece = window.bobj.getpiece({ x, y });
+
+                if (piece) {
+                    if (piece.color !== this.color) {
+                        possibleMoves.push({ x, y }); // Can capture the opponent's piece
+                    }
+                    break; 
+                  }
+
+                possibleMoves.push({ x, y });
+
+                 x += dx[i];
+                y += dy[i];
+            }
+        }
+
+        return possibleMoves;
     }
 }
+
 export default Queen;
