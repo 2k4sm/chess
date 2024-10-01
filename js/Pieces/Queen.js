4d540cab178433dd5598291858fe353450521f08
spoorthy madasu <spoorthymadasu@spoorthys-MacBook-Air.local>
2024-10-01 10:01:36 +0530

    Updated code

--- js/Pieces/Queen.js
+++ js/Pieces/Queen.js
@@ -1,39 +1,41 @@
 import { posinbounds } from '../utility.js';
 import Piece from "./Piece.js";
 
-class Queen extends Piece {
-    constructor(color, pos) {
-        super(color, pos);
-        this.type = 'queen';
-    }
-
-    moves() {
-        let possibleMoves = [];
-        let dx = [1, 1, -1, -1, 1, 0, -1, 0];
-        let dy = [1, -1, 1, -1, 0, 1, 0, -1];
-     for (let i = 0; i < 8; i++) {
-            let x = this.pos.x + dx[i];
-            let y = this.pos.y + dy[i];
-
-            while (posinbounds({ x, y })) {
-                let piece = window.bobj.getpiece({ x, y });
-
-                if (piece) {
-                    if (piece.color !== this.color) {
-                        possibleMoves.push({ x, y }); // Can capture the opponent's piece
-                    }
-                    break; 
-                  }
-
-                possibleMoves.push({ x, y });
-
-                 x += dx[i];
-                y += dy[i];
-            }
-        }
-
-        return possibleMoves;
-    }
+function Queen(color, pos) {
+    Piece.call(this, color, pos);
+    this.type = 'queen';
 }
+
+Queen.prototype = Object.create(Piece.prototype);
+Queen.prototype.constructor = Queen;
+
+Queen.prototype.moves = function() {
+    let possibleMoves = [];
+    let dx = [1, 1, -1, -1, 1, 0, -1, 0];
+    let dy = [1, -1, 1, -1, 0, 1, 0, -1];
+
+    for (let i = 0; i < 8; i++) {
+        let x = this.pos.x + dx[i];
+        let y = this.pos.y + dy[i];
+
+        while (posinbounds({ x, y })) {
+            let piece = window.bobj.getpiece({ x, y });
+
+            if (piece) {
+                if (piece.color !== this.color) {
+                    possibleMoves.push({ x, y });
+                }
+                break;
+            }
+
+            possibleMoves.push({ x, y });
+
+            x += dx[i];
+            y += dy[i];
+        }
+    }
+
+    return possibleMoves;
+};
 
 export default Queen;
