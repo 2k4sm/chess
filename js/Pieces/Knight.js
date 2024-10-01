74778dad32292600635bcac5bd5544e72b5870ba
Anant Shukla <anantshukla404@gmail.com>
2024-10-01 11:16:26 +0530

    update knight for prototypes

--- js/Pieces/Knight.js
+++ js/Pieces/Knight.js
@@ -1,37 +1,37 @@
-import Piece from './Piece.js';
 import { posinbounds } from '../utility.js';
 
-class Knight extends Piece {
-    constructor(color, pos) {
-        super(color, pos);
-        this.type = 'knight';
-    }
+function Knight(color, pos) {
+    Piece.call(this, color, pos);  // Inherit properties from Piece
+    this.type = 'knight';
+}
 
-    moves() {
-        let possibleMoves = [];
+Knight.prototype = Object.create(Piece.prototype);
+Knight.prototype.constructor = Knight;
 
-        let directions = [
-            { dx: -2, dy: -1 }, { dx: -2, dy: 1 },
-            { dx: -1, dy: -2 }, { dx: -1, dy: 2 },
-            { dx: 1, dy: -2 }, { dx: 1, dy: 2 },
-            { dx: 2, dy: -1 }, { dx: 2, dy: 1 }
-        ];
+Knight.prototype.moves = function() {
+    let possibleMoves = [];
 
-        for(let dir of directions){
-            let x = this.pos.x + dir.dx;
-            let y = this.pos.y + dir.dy;
+    let directions = [
+        { dx: -2, dy: -1 }, { dx: -2, dy: 1 },
+        { dx: -1, dy: -2 }, { dx: -1, dy: 2 },
+        { dx: 1, dy: -2 }, { dx: 1, dy: 2 },
+        { dx: 2, dy: -1 }, { dx: 2, dy: 1 }
+    ];
 
-            if(posinbounds({ x, y })){
-                let piece = window.bobj.getpiece({ x, y });
+    for(let dir of directions){
+        let x = this.pos.x + dir.dx;
+        let y = this.pos.y + dir.dy;
 
-                if(!piece || piece.color !== this.color){
-                    possibleMoves.push({ x, y });
-                }
+        if(posinbounds({ x, y })){
+            let piece = window.bobj.getpiece({ x, y });
+
+            if(!piece || piece.color !== this.color){
+                possibleMoves.push({ x, y });
             }
         }
-
-        return possibleMoves;
     }
-}
+
+    return possibleMoves;
+};
 
 export default Knight;
