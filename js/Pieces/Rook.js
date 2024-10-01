635ab02740f783007a956001257a19347b0471d9
BHAV0207 <bhavyajain4274@gmail.com>
2024-10-01 10:03:50 +0530

    chnaged the es6 class based pieces to prototype

--- js/Pieces/Rook.js
+++ js/Pieces/Rook.js
@@ -1,34 +1,38 @@
-import Piece from "./Piece.js";
 import { posinbounds } from '../utility.js';
-class Rook extends Piece {
-    constructor(color, pos) {
-        super(color, pos);
-        this.type = 'rook';
-    }
-    moves() {
-        let possibleMoves = [];
-        let dx = [1, 0, -1, 0];
-        let dy = [0, 1, 0, -1];
-        for (let i = 0; i < 4; i++)
-        {
-            let x = this.pos.x + dx[i];
-            let y = this.pos.y + dy[i];
-            while (posinbounds({x, y}))
-            {
-                let piece = window.bobj.getpiece({x, y});
-                if(piece)
-                {
-                    if(piece.color !== this.color)
-                        possibleMoves.push({x, y});
-                    break;
-                }
-                possibleMoves.push({x, y});
-                x += dx[i];
-                y += dy[i];
-            }
-        }
-        return possibleMoves;   
-    }
+
+function Rook(color, pos) {
+    Piece.call(this, color, pos); // Call the parent constructor
+    this.type = 'rook';
 }
+
+// Inherit from Piece
+Rook.prototype = Object.create(Piece.prototype);
+Rook.prototype.constructor = Rook;
+
+// Define the moves method on the prototype
+Rook.prototype.moves = function() {
+    let possibleMoves = [];
+    let dx = [1, 0, -1, 0];
+    let dy = [0, 1, 0, -1];
+    
+    for (let i = 0; i < 4; i++) {
+        let x = this.pos.x + dx[i];
+        let y = this.pos.y + dy[i];
+        
+        while (posinbounds({x, y})) {
+            let piece = window.bobj.getpiece({x, y});
+            if (piece) {
+                if (piece.color !== this.color) {
+                    possibleMoves.push({x, y});
+                }
+                break;
+            }
+            possibleMoves.push({x, y});
+            x += dx[i];
+            y += dy[i];
+        }
+    }
+    return possibleMoves;
+};
 
 export default Rook;
